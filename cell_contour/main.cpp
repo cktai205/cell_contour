2e149be78f5cb2c46c9e61b328782992656e5ce2
Calvin Tai <ckcalvin1997@gmail.com>
2018-06-19 15:26:43 +0800

    Initial Commit

---
+++ cell_contour/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  cell_contour
+//
+//  Created by Calvin CK Tai on 6/19/18.
+//  Copyright © 2018 intern. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
