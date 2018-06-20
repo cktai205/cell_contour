894179e7d6b52d1989c3256209c4dae8f8598046
Calvin <ckcalvin1997@hotmail.com>
2018-06-20 12:52:04 +0800

    Basic Cell Contour
    Initial cell contouring with no filter.

--- cell_contour/main.cpp
+++ cell_contour/main.cpp
@@ -12,21 +12,22 @@
 #include "opencv2/opencv.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/imgproc.hpp"
+#include "opencv2/highgui.hpp"
 
 using namespace cv;
 int main()
 {
     
+    // 读取图片
     Mat image;
     image = imread("/Users/cktai/desktop/cell.jpeg");
-//    namedWindow("Display Image", WINDOW_AUTOSIZE );
-//    imshow("Display Colored Image", image);
     
+    // 将图片灰化
     Mat gray;
     cvtColor(image,gray,COLOR_BGR2GRAY);
-//    namedWindow("Gray Image", WINDOW_AUTOSIZE);
-//    imshow("Display Gray Image", gray);
+
     
+    // 取轮廓及描绘轮廓
     Mat bin_image;
     threshold(gray,bin_image,128,255,THRESH_OTSU);
     std::vector<std::vector<Point>> contours;
@@ -37,7 +38,8 @@ int main()
     namedWindow("Unmarked result",WINDOW_AUTOSIZE);
     imshow("Result unmarked",result);
     drawContours(result,contours,-1, Scalar(0,255,0));
-    
+
+    // 写出结果
     namedWindow("Contours", WINDOW_AUTOSIZE);
     imshow("Contours", result);
     imwrite("/Users/cktai/desktop/result.jpeg",result);
